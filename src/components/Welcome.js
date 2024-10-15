182e5702bd547a41a48fd5d7dc794b3ba7bf2b6b
munna-sed <munna@sportainmentdesign.com>
2024-10-15 12:39:06 +0530

    Create theme
    Created theme on 15-Oct-2024

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+function Welcome() {
+  return (          
+		 <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 class="font-weight-bold">Welcome SED</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+  );
+}
+export default Welcome;
